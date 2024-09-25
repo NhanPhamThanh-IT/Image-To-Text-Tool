6658ede3134a68ecea68a6daea5290fb0945cc08
Nhan Pham Thanh <ptnhanit230104@gmail.com>
2024-09-25 20:04:08 +0700

    Update to render the content after using OCR to the front-end

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
 import pytesseract
 import os
 from utils.usingOCR import OCR
@@ -6,7 +7,7 @@ from utils.support import allowed_file
 
 app = Flask(__name__)
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\NhanPham\AppData\Local\Programs\Tesseract-OCR'
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -16,17 +17,24 @@ def main_page():
     return render_template('home.html')
 
 @app.route('/upload', methods=['POST'])
-def upload_file():
+def take_local_image():
     if 'image' not in request.files:
-        return render_template('home.html', warning='No file part !')
+        return render_template('home.html', warning='No file part!')
     file = request.files['image']
     if file.filename == '':
-        return render_template('home.html', warning='No selected file !')
+        return render_template('home.html', warning='No selected file!')
     if file and allowed_file(file.filename):
-        filename = file.filename
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return render_template('home.html', warning='File uploaded successfully !')
-    return render_template('home.html', warning='File type not allowed !')
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        model = OCR(file_path)
+        os.remove(file_path)
+        os.removedirs(UPLOAD_FOLDER)
+        content = model.get_text(pytesseract)
+        if not content:
+            content = 'Unable to recognize text for this image!'
+        return render_template('home.html', content=content)
+    return render_template('home.html', warning='File type not allowed!')
 
 if __name__ == "__main__":
     if not os.path.exists("uploads"):
